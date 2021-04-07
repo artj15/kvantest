c7aad3015e65f4713f4580629a14b3b8cc4adddd
artj15 <artj15@gmail.com>
2021-04-07 16:13:19 +0300

    1 commit

--- kvando/models.py
+++ kvando/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=15)
+    first_name = models.CharField(max_length=50, default= 'Имя')
+    last_name = models.CharField(max_length=50, default= 'Фамилия')
+    phone_number = models.CharField(max_length=15, 'Номер телефона')
